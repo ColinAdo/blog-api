68f2d0ce947c7a23731e4d575d9d8b6bee2f9b25
ColinAdo <colinadodev30@gmail.com>
2024-01-26 09:06:09 +0300

    Add corsheader to INSTALLED_APP and some CORS and CSRF configs

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Third party 
     'rest_framework',
+    'corsheader',
 
     # Local
     'accounts.apps.AccountsConfig',
@@ -55,6 +56,7 @@ REST_FRAMEWORK = {  # new
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheader.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -134,5 +136,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+]
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
