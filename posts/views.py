68eee79e0f306285aafef2c940193d384320c6b2
ColinAdo <colinadodev30@gmail.com>
2024-01-27 08:02:25 +0300

    Add custom api permisssions to views

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,18 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import generics # permissions => view level permissions
 
 from .serializers import PostSerializer
 from .models import Post
+from .permissions import IsAuthorOrReadOnly  # => custom  permissions
 
-class ListPost(ListCreateAPIView):
+
+class ListPost(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class DetailPost(RetrieveUpdateDestroyAPIView):
+class DetailPost(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAdminUser,) => view level permissions
+    permission_classes = (IsAuthorOrReadOnly,)  # => custom  permissions
     queryset = Post.objects.all()
     serializer_class = PostSerializer
