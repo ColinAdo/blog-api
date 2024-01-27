d1f021d856cfac53bbcf2ec2576d4f77a7842637
ColinAdo <colinadodev30@gmail.com>
2024-01-27 11:17:08 +0300

    Add allauth to INSTALLED_APPS

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,13 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # Third party 
     'rest_framework',
     'corsheaders',
     'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
     'dj_rest_auth',
+    'dj_rest_auth.registration',
 
+    'allauth.socialaccount',
     # Local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
@@ -68,6 +73,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'django_project.urls'
@@ -83,10 +90,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
