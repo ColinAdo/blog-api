bf0fd6ce9ae11d83f3d6d9aa7512fc125f302405
ColinAdo <colinadodev30@gmail.com>
2024-01-27 11:18:39 +0300

    Add signals to the app Conf

--- posts/apps.py
+++ posts/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'posts'
+
+    def ready(self):
+        import posts.signals
+
+
+# fa9f6409e47f789ea06c40334c7af991f6a14dfe
+# fa9f6409e47f789ea06c40334c7af991f6a14dfe
