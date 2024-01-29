1585ad2d151837fbd718a51954909ccafe66185d
ColinAdo <colinadodev30@gmail.com>
2024-01-29 09:34:18 +0300

    Add viewset for users and posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+"""
 from django.contrib.auth import get_user_model
 from rest_framework import generics # permissions => view level permissions
 
@@ -24,5 +25,25 @@ class ListUser(generics.ListCreateAPIView):
 
 
 class DetailUser(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+"""
+
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+
+from .models import Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer, UserSerializer
+
+class PostViewset(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly, )
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
