ca86daa9e728a702a6764fd37c6a4c145c585ae8
ColinAdo <colinadodev30@gmail.com>
2024-01-29 09:07:43 +0300

    Add user urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ListPost, DetailPost
+from .views import ListPost, DetailPost, ListUser, DetailUser
 
 urlpatterns = [
+    path('users/', ListUser.as_view(), name='list_user'),
+    path('users/<str:pk>/', DetailUser.as_view(), name='detail_user'),
     path('<str:pk>/', DetailPost.as_view(), name='detail_post'),
     path('', ListPost.as_view(), name='list_post'),
 ]
