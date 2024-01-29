ef19f9fb94a24cc4ce50114c2ad85e84cac9ea51
ColinAdo <colinadodev30@gmail.com>
2024-01-29 09:07:20 +0300

    Add user views

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics # permissions => view level permissions
 
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .models import Post
 from .permissions import IsAuthorOrReadOnly  # => custom  permissions
 
@@ -16,3 +17,12 @@ class DetailPost(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)  # => custom  permissions
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class ListUser(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class DetailUser(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
